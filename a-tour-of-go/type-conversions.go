11f3d7276db7f712b7c1f8728136022029d33e5d
Swapnil Singh <swappp7singh@gmail.com>
2016-10-31 08:58:10 +0530

    type conversions and type inference

---
+++ a-tour-of-go/type-conversions.go
@@ -0,0 +1,9 @@
+package main 
+
+import "fmt"
+
+func main() {
+	var i int = 42
+	var f float64 = float64(i)
+	var u uint = uint(f)
+}
---
+++ a-tour-of-go/type-inference.go
@@ -0,0 +1,9 @@
+package main 
+
+import "fmt"
+
+func main() {
+	var i int
+	j := i
+	fmt.Printf("j is of type %T\n", j)
+}
